a6f8ebb40f8e90c9d549f2705317d478d5f0fca4
vaishalimaheshwari2000 <vaishali.maheshwari4180@gmail.com>
2023-05-08 22:03:34 +0530

    changes

--- CASpartA(new).php
+++ CASpartA(new).php
@@ -307,9 +307,9 @@ tr:nth-child(even) {
             while($rows = mysqli_fetch_assoc($dataArray1)){ 
                 $data_array1[] = $rows; 
             }
-              echo"text1";               
+                        
             foreach($data_array1 as $showData1){ 
-                echo"text3"; 
+               
         ?>
 
         <tr>
